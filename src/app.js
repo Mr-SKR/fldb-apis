0d546b128cb08872fbcc750438ce4b2eecda8899
Suresh Reddy <sureshreddy@engineer.com>
2022-01-08 02:03:45 +0530

    update cors headers

--- src/app.js
+++ src/app.js
@@ -95,7 +95,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Set CORS headers
 app.use((req, res, next) => {
-  res.append("Access-Control-Allow-Origin", ["http://localhost:3000"]);
+  res.append("Access-Control-Allow-Origin", ["https://fldb.in"]);
   res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.append("Access-Control-Allow-Headers", "Content-Type");
   next();
